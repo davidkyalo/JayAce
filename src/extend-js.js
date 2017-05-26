3cd95dad5a18b07a023780e2e5b9114364aa0a4c
David Kyalo <davidmkyalo@gmail.com>
2017-05-26 03:56:54 +0300

    Extended some core js features.

---
+++ src/extend-js.js
@@ -0,0 +1,55 @@
+define([], function(){
+	String.prototype.join = function(){
+		var g = this;
+		var j = null;
+
+		if(arguments.length == 1 && typeof(arguments[0]) === 'object')
+			var pieces = arguments[0];
+		else
+			var pieces = arguments;
+
+		for (var i = 0; i < pieces.length; i++) {
+			var p = pieces[i].toString();
+			j = j == null ? p : j+g+p;
+
+		}
+		return j;
+	}
+
+	String.prototype.pad = function(value, len){
+		var self = this;
+		var d = len < 0 ? (len*-1) - this.length : len - this.length;
+		if(d < 1)
+			return this;
+		var p = value.repeat(d);
+		return len > 0 ? this+p : p+this;
+	}
+
+	if((new Object()).__class__ === undefined){
+		var descriptor = {
+			get : function(){
+				return this.__proto__.constructor;
+			},
+			enumerable : false,
+			configurable : false
+		};
+
+		Object.defineProperty(Object.prototype, '__class__', descriptor);
+	}
+
+	if(Element.prototype.remove === undefined){
+		Element.prototype.remove = function() {
+			this.parentElement.removeChild(this);
+		};
+
+		NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+			for(var i = this.length - 1; i >= 0; i--) {
+				if(this[i] && this[i].parentElement) {
+					this[i].parentElement.removeChild(this[i]);
+				}
+			}
+		};
+	}
+
+
+});
