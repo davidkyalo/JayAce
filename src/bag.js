41978f64230754e0ae85a908e1cdf7fc0c12e19f
David Kyalo <davidmkyalo@gmail.com>
2017-07-01 15:09:07 +0300

    Each bag object uses a it's own instance of obj so as to support custom notations

--- src/bag.js
+++ src/bag.js
@@ -8,23 +8,31 @@ define([
 
 	class Bag
 	{
-		constructor(items, missing){
-			if(items) this.update(items);
-
+		constructor(items, missing, notation='.'){
+	
 			Object.defineProperty(this, '__missing__', {
 				enumerable: false,
 				configurable: false,
 				writable: false,
 				value: missing
 			});
+
+			Object.defineProperty(this, '__oO__', {
+				enumerable: false,
+				configurable: false,
+				writable: true,
+				value: new obj.Obj(notation)
+			});
+
+			if(items) this.update(items);
 		}
 
 		has (key){
-			return obj.has(this, key);
+			return this.__oO__.has(this, key);
 		}
 
 		get (key, _default){
-			var rv = obj.get(this, key, _default);
+			var rv = this.__oO__.get(this, key, _default);
 			if(rv === undefined && this.__missing__){
 				rv = this.__missing__(this, key);
 				if(rv !== undefined)
@@ -43,11 +51,11 @@ define([
 		}
 
 		set (key, value, _default){
-			return obj.set(this, key, value, _default);
+			return this.__oO__.set(this, key, value, _default);
 		}
 
 		default (key, _default=null){
-			var value = obj.get(this, key, NOTHING);
+			var value = this.__oO__.get(this, key, NOTHING);
 			if(value === NOTHING){
 				value = _default;
 				this.set(key, value);
@@ -56,11 +64,11 @@ define([
 		}
 
 		defaults (defaults){
-			obj.extend(true, this, defaults, this.copy(true));
+			this.__oO__.extend(true, this, defaults, this.copy(true));
 		}
 
 		forget (key){
-			return obj.forget(this, key);
+			return this.__oO__.forget(this, key);
 		}
 
 		remove (keys){
@@ -76,7 +84,7 @@ define([
 			}
 
 			if(items.length > 2)
-				obj.extend.apply(obj, items);
+				this.__oO__.extend.apply(this.__oO__, items);
 		}
 
 		merge (/* , items... */){
@@ -86,19 +94,19 @@ define([
 		}
 
 		each(callback){
-			return obj.each(this, callback);
+			return this.__oO__.each(this, callback);
 		}
 
 		copy (deep=false){
-			return new Bag(obj.copy(this, deep));
+			return new Bag(this.__oO__.copy(this, deep));
 		}
 
 		keys (){
-			return obj.keys(this);
+			return this.__oO__.keys(this);
 		}
 
 		count (){
-			return obj.count(this);
+			return this.__oO__.count(this);
 		}
 	}
 
